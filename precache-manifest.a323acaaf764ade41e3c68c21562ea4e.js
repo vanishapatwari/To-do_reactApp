661a90bbc28b75eca30d2a88d0577c6070d1add6
unknown <ipriyanshugautam@gmail.com>
2021-09-10 23:17:14 +0530

    Updates

---
+++ precache-manifest.a323acaaf764ade41e3c68c21562ea4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cdb903ef5690ca012be7615f685c382",
+    "url": "/To-do_reactApp/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/To-do_reactApp/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "d8b3906e5e66ebbe7c50",
+    "url": "/To-do_reactApp/static/js/main.4d464d5d.chunk.js"
+  },
+  {
+    "revision": "8e8093e958699b0849a6",
+    "url": "/To-do_reactApp/static/js/runtime-main.e1bb7995.js"
+  }
+]);
